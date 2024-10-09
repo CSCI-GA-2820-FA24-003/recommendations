477e270f8f4fd1a6c6d2790eb7a99f90dc789caa
Zoey367 <yg3363@nyu.edu>
2024-10-09 13:56:33 +0000

    Your commit message

--- tests/factories.py
+++ tests/factories.py
@@ -16,8 +16,8 @@ class RecommendationsFactory(factory.Factory):
         model = Recommendations
 
     id = factory.Sequence(lambda n: n)
-    product_id = factory.Sequence(lambda n: n + 100)
-    recommended_id = factory.Sequence(lambda n: n + 200)
+    product_id = factory.Sequence(lambda n: n)
+    recommended_id = factory.Sequence(lambda n: n + 100)
     status = FuzzyChoice(choices=["active", "expired", "draft"])
     type = FuzzyChoice(choices=["cross-sell", "up-sell", "accessory"])
 
